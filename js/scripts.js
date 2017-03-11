1328f23b9ee8946358745f6a130781095a5515bb
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-03-10 18:59:34 -0800

    fix output issue, basics completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 var divisibility = function(number) {
 
   for (index = 1; index <= number; index ++) {
-    if ((number % 3 === 0) && (number % 5 === 0)) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
     $("#output").append("<li> pingpong </li>");
-  } else if (number % 5 === 0) {
+  } else if (index % 5 === 0) {
     $("#output").append("<li> pong </li>");
-  } else if (number % 3 === 0) {
+  } else if (index % 3 === 0) {
     $("#output").append("<li> ping </li>");
   } else {
-    $("#output").append("<li>" + number + "</li>");
+    $("#output").append("<li>" + index + "</li>");
   }
 }
 };
